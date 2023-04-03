6c7637aad5e6c1426caddc0d07164c1490e27736
Simonas Pranaitis <simopnas.pranaitis@gmail.com>
2023-04-03 14:54:10 +0300

    mad libs

---
+++ madLibs/main.py
@@ -0,0 +1,7 @@
+color = input("Enter color: ")
+plural_noun = input("Enter plural noun: ")
+celebrity = input("Enter celebrity name: ")
+
+print("Roses are " + color.capitalize())
+print(plural_noun.capitalize() + " ale blue, ")
+print("I love " + celebrity.capitalize() + "!")
