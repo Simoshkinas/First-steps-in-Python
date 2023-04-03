5c603535c9f8e2f3d15b72975793f0c858742cd7
Simonas Pranaitis <simopnas.pranaitis@gmail.com>
2023-04-03 14:36:23 +0300

    basic conversion from str to float

---
+++ basicCalc/main.py
@@ -0,0 +1,13 @@
+num1 = input("Enter first number: ")
+num2 = input("Enter second number: ")
+num1 = float(num1)
+num2 = float(num2)
+result1 = num1 + num2
+result2 = num1 - num2
+result3 = num1 * num2
+result4 = num1 / num2
+
+print(result1)
+print(result2)
+print(result3)
+print(result4)
