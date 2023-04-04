fcd9cd868206714d92dc2dbbdd0c614603c118f7
Simonas Pranaitis <simopnas.pranaitis@gmail.com>
2023-04-04 15:00:41 +0300

    Basic functions

---
+++ pythonFunctions/main.py
@@ -0,0 +1,15 @@
+def say_hi():
+    print("Hello, User!")
+
+
+def say_hi2(name):
+    print("Hello, " + name + "!")
+
+
+def say_hi_with_age(name, age):
+    print("Hello, " + name + "!, You are " + str(age))
+
+
+say_hi()
+say_hi2("Han")
+say_hi_with_age("John", 50)
