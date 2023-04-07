a4871db40e6fb31b2512a20b44a42eaa1bd0aac1
Simonas Pranaitis <simopnas.pranaitis@gmail.com>
2023-04-07 16:13:21 +0300

    Basic for loops

---
+++ pythonForLoop/main.py
@@ -0,0 +1,22 @@
+for letter in "string":
+    print(letter)
+
+
+friends = ["Tom", "Jim", "Jason"]
+for friend in friends:
+    print(friend)
+
+for index in range(10):
+    print(index)
+
+for index in range (3, 10):
+    print(index)
+
+for index in range(len(friends)):
+    print(friends[index])
+
+for index in range(5):
+    if index == 0:
+        print("First iteration")
+    else:
+        print("Not first iteration")
