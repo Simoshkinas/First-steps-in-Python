0d7cdb15fe0c2c56d6f795286f83a2a7125b7560
Simonas Pranaitis <simopnas.pranaitis@gmail.com>
2023-04-12 13:29:15 +0300

    Basic exeption cathcing

---
+++ pythonTryExcept/main.py
@@ -0,0 +1,8 @@
+try:
+    # result = 10 / 0
+    number = int(input("enter a number: "))
+    print(number)
+except ZeroDivisionError as err:
+    print(err)
+except ValueError:
+    print("Incorrect input")
