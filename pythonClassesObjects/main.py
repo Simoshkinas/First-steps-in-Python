91e6b91a7aa3971a2f6391f7b504013fc2cb30bf
Simonas Pranaitis <simopnas.pranaitis@gmail.com>
2023-04-12 16:08:45 +0300

    Example of a basic class

---
+++ pythonClassesObjects/main.py
@@ -0,0 +1,14 @@
+from student import Student
+
+student1 = Student("Jim", "Business", 3.7, False)
+student2 = Student("Pam", "Art", 2.4, True)
+
+print(student1.name)
+print(student1.major)
+print(student1.gpa)
+print(student1.is_on_probation)
+
+print(student2.name)
+print(student2.major)
+print(student2.gpa)
+print(student2.is_on_probation)
