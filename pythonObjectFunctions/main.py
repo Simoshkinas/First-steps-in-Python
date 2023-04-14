ac44da110cdd635984d458a0a69f560b757351fb
Simonas Pranaitis <simopnas.pranaitis@gmail.com>
2023-04-14 14:50:14 +0300

    Example of using function of a particular class

---
+++ pythonObjectFunctions/main.py
@@ -0,0 +1,7 @@
+from Student import Student
+
+student1 = Student("Oscar", "Accountant", 3.8)
+student2 = Student("Phillis", "Business", 2.3)
+
+print(student1.on_honor_roll())
+print(student2.on_honor_roll())
