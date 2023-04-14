858a9af43a1bb34f946d34f9058906ded8c70416
Simonas Pranaitis <simopnas.pranaitis@gmail.com>
2023-04-14 15:30:41 +0300

    Example of class inheritance and function override

---
+++ pythonInheritance/main.py
@@ -0,0 +1,14 @@
+from Chef import Chef
+from ChineseChef import ChineseChef
+
+myChef = Chef()
+myChef.make_chicken()
+myChef.make_salad()
+myChef.make_special_dish()
+
+myChineseChef = ChineseChef()
+myChineseChef.make_salad()
+myChineseChef.make_fried_rice()
+
+myChef.make_special_dish()
+myChineseChef.make_special_dish()
